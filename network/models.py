bc2a16ea13e9d6a6663c8c01d61f17022a375612
David Stansbury <davidstansbury@Davids-MacBook-Air.local>
2023-10-06 11:36:39 +0300

    creates post, like and follow models

--- network/models.py
+++ network/models.py
@@ -4,3 +4,36 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    poster = models.ForeignKey("User", on_delete=models.CASCADE, related_name="posts")
+    body = models.TextField(blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "poster": self.poster,
+            "body": self.body,
+            "timestamp": self.timestamp.strftime("%b %d %Y, %I:%M %p")
+        }
+    
+class Follow(models.Model):
+    follower = models.ForeignKey("User", on_delete=models.CASCADE, related_name="following")
+    follows = models.ForeignKey("User", on_delete=models.CASCADE, related_name="followers")
+    
+    def serialize(self):
+        return {
+            "follower": self.follower,
+            "follows": self.follows 
+        }
+    
+class Like(models.Model):
+    liker = models.ForeignKey("User", on_delete=models.CASCADE, related_name="likes")
+    post = models.ForeignKey("Post", on_delete=models.CASCADE, related_name="likes")
+    
+    def serialize(self):
+        return {
+            "liker": self.liker,
+            "post": self.post
+        }
